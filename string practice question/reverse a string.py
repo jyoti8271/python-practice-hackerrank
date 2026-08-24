500640abbbf3b7acf9781ab2b3d2a2bc682aa89a
jyoti8271 <jyotikumari8271@gmail.com>
2026-08-24 13:45:34 +0530

    linear search

--- string practice question/reverse a string.py
+++ string practice question/reverse a string.py
@@ -1,3 +1,11 @@
+# s=str(input("enter the string"))
+# reverse=s[: : -1]
+# print(reverse)
+
+
 s=str(input("enter the string"))
-reverse=s[: : -1]
+reverse=""
+for i in range(len(s)-1, -1, -1):
+    reverse += s[i]
+    
 print(reverse)
